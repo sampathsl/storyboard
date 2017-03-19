af54f0e07d43cc12d072b867c908b2297aa28b71
sampathsl <sampath.thennakoon@gmail.com>
2017-03-19 08:45:12 +0530

    Add E-tag header : versioning the REST urls Add REST API versioning

--- src/main/java/com/sampathsl/storybook/jpa/model/BaseEntity.java
+++ src/main/java/com/sampathsl/storybook/jpa/model/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.sampathsl.storybook.jpa.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -11,6 +8,8 @@ public abstract class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Version
+    private Long version;
 
 	protected BaseEntity() {}
 
