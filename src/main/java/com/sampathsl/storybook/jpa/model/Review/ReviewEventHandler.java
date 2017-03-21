574d77789ea83d1106991e51648d4d72d634f309
Sampath Thennakoon <sampath@intervest.gi>
2017-03-21 15:38:34 +0530

    Add review user

---
+++ src/main/java/com/sampathsl/storybook/jpa/model/Review/ReviewEventHandler.java
@@ -0,0 +1,28 @@
+package com.sampathsl.storybook.jpa.model.Review;
+
+import com.sampathsl.storybook.jpa.model.user.User;
+import com.sampathsl.storybook.jpa.model.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by SAMPATH on 3/21/2017.
+ */
+@Component
+@RepositoryEventHandler(Review.class)
+public class ReviewEventHandler {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @HandleBeforeCreate
+    public void addReviewerBasedOnLoggedInUser(Review review) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUserName(userName);
+        review.setUser(user);
+    }
+
+}
